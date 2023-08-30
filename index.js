62ee30bc49d7d44cab43f02b6f1d2e81c924703c
pratikshaghodke <pratikshaghodke6@gmail.com>
2023-08-30 15:07:52 +0530

    cors update

--- index.js
+++ index.js
@@ -13,26 +13,26 @@ const fs = require('fs');
 //   })
 // );
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-})
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// })
 
 
-// const allowedOrigins = ['https://pratikshaghodke.github.io/'];
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if(!origin) return callback(null, true);
-//     if(allowedOrigins.indexOf(origin) === -1){
-//       var msg = 'The CORS policy for this site does not ' +
-//                 'allow access from the specified Origin.';
-//       return callback(new Error(msg), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
+const allowedOrigins = ['https://pratikshaghodke.github.io/AdhiShaktiAdhiMaya'];
+app.use(cors({
+  origin: function (origin, callback) {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // const allowedOrigins = ['https://pratikshaghodke.github.io/'];
 // const corsOptions = {
