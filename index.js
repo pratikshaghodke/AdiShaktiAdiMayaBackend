97900f873ff3b8bcd8b4abf2c0163635d0ee0dd5
pratikshaghodke <pratikshaghodke6@gmail.com>
2023-08-30 12:58:32 +0530

    cors update

--- index.js
+++ index.js
@@ -13,20 +13,20 @@ const fs = require('fs');
 //   })
 // );
 
-const corsOptions = {
-  origin: '*',
-};
 
-// const allowedOrigins = ['https://pratikshaghodke.github.io/'];
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   }
-// }));
+
+const allowedOrigins = ['https://pratikshaghodke.github.io/'];
+app.use(cors({
+  origin: function (origin, callback) {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 
 mongoose.connect(
